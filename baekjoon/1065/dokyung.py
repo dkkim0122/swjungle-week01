397dcc582c16e51f652264a639541a7c26cbd6de
dkkim0122 <dkkim0122@gmail.com>
2021-11-06 14:39:26 +0900

    1065 문제 해결. map()으로 정수 각 자리 리스트화 가능, 함수에서 return 남발하지 않기

--- baekjoon/1065/dokyung.py
+++ baekjoon/1065/dokyung.py
@@ -1,22 +1,13 @@
-def int_to_list(a):
-    lst = []
-    while a != 0:
-        num = a % 10
-        a = a // 10
-        lst.append(num)
-    return lst
-
-
 def find_han(n):
-    if n >= 1 and n < 100:
-        return n
-    else:
-        count = 99
-        for i in range(100, n+1):
-            a = int_to_list(i)
-            if a[1] == (a[0]+a[2])/2:
-                count += 1
-        
+    count = 0
+
+    for i in range(1, n+1):
+        a = list(map(int, str(i)))
+        if i >= 1 and i < 100:
+            count += 1
+        elif a[1] == (a[0]+a[2])/2:
+            count += 1
+            
     return count
 
 if __name__ == '__main__':
