4deada8e46470ae2e38eee130983b55b9fd9f641
dkkim0122 <dkkim0122@gmail.com>
2021-11-06 21:06:27 +0900

    2562 문제 수정. 변수는 헷갈리게 놓지 말것.

--- baekjoon/2562/dokyung.py
+++ baekjoon/2562/dokyung.py
@@ -1,13 +1,13 @@
 # 자연수들을 받으면서 최댓값을 계속 비교하기. 중간에 몇 번째인지도 수정하기
 
 max = 0
-index = 0
+num = 0
 
 for i in range(9):
     a = int(input())
     if a > max:
         max = a
-        index = i + 1
+        num = i + 1
 
 print(max)
-print(index)
+print(num)
