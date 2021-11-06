6ce0803b6e96746f42684d42ee0d11e2d4b88a5a
dkkim0122 <dkkim0122@gmail.com>
2021-11-06 21:41:05 +0900

    10871 문제 수정.

--- baekjoon/10871/dokyung.py
+++ baekjoon/10871/dokyung.py
@@ -1,5 +1,5 @@
 # 첫째 줄에 N과 X가 주어진다. (1 ≤ N, X ≤ 10,000)
-# 굳이 입력받은 N을 가지고 for문을 돌릴 필요는 없다.
+# print(f'{a}', end='') == print(a, end='')
 
 import sys
 
@@ -9,4 +9,4 @@ lst = list(map(int, input().split()))
 
 for a in lst:
     if a < x:
-        print(f'{a} ', end='')
+        print(a, end='')
