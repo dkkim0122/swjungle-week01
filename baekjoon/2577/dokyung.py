dd6e576be5cf3823fe20abe7fa17a8aed44e1bd3
dkkim0122 <dkkim0122@gmail.com>
2021-11-06 21:38:28 +0900

    2577 문제 수정. count() 함수 적용해보기

--- baekjoon/2577/dokyung.py
+++ baekjoon/2577/dokyung.py
@@ -1,5 +1,4 @@
-# 빈 리스트를 하나 만들어두고, 
-# 나누었을 때 나머지 == 자리의 숫자 에 해당하는 인덱스에 1을 더해 준다.
+# count()함수 사용해보기
 
 lst = [0 for i in range(10)] 
 
@@ -9,10 +8,7 @@ c = int(input())
 
 multi = a * b * c
 
-while multi != 0:
-    num = int(multi % 10)
-    multi = multi // 10
-    lst[num] += 1
+str_multi = str(multi)
 
 for i in range(10):
-    print(lst[i])
+    print(str_multi.count(str(i)))
