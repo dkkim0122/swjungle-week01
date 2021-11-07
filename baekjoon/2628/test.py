f91ec5d69d8ad349fe03f9eb7214b337e29dc3af
dkkim0122 <dkkim0122@gmail.com>
2021-11-07 16:26:22 +0900

    * 9020 문제 해결. 다시 한번 더 봐야한다.c:/Users/Kim Dokyung/swjungle/swjungle-week01/.venv/Scripts/python.exe

--- baekjoon/2628/test.py
+++ baekjoon/2628/test.py
@@ -1,3 +1,12 @@
-a = [0,10]
+n = int(input())
+is_prime = [False, False] + [True]*(n-1)    # 0과 1은 소수가 아니다
+primes = []
 
-print(a)
+sq = int(n**0.5)
+
+for i in range(2, sq+1):
+    if is_prime[i]:    # 만약 해당 숫자가 소수이면 소수 리스트에 추가한다.
+        primes.append(i)
+        for j in range(2*i, n+1, i):    # 그리고 해당 숫자(소수)의 배수들은 모두 소수가 아니다.
+            is_prime[j] = False
+print(primes)
