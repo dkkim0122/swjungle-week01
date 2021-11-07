1925c1b33a30093ef2425e23d107d807a76c0054
dkkim0122 <dkkim0122@gmail.com>
2021-11-07 16:53:45 +0900

    8958 문제 해결. flag 사용하지 않고 더 간결한 풀이 활용

--- baekjoon/8958/dokyung.py
+++ baekjoon/8958/dokyung.py
@@ -1,31 +1,19 @@
 # 테스트 케이스 개수를 입력받으면 그 개수만큼 for문을 돌린다. 
-# flag 활용???
+import sys
+input = sys.stdin.readline
+
 n = int(input())
+lst = list(input().strip() for _ in range(n))
 
-for i in range(n):
-    lst = list(input())
-    flag = False
-
+for a in lst:
     score = 0
     add_score = 0
-    total = 0
 
-
-    for i in range(len(lst)):
-        if flag == False and lst[i] == 'O':
-            flag = True
+    for i in range(len(a)):
+        if a[i] == 'O':
             score += 1
             add_score += score
-        elif flag == True and lst[i] == 'O':
-            score += 1
-            add_score += score
-        elif flag == True and lst[i] == 'X':
-            flag = False
-            total += add_score
+        else:
             score = 0
-            add_score = 0
-
-    total += add_score
-
-    print(total)
-
+    print(add_score)
+    
---
+++ baekjoon/9020/정수론.py
@@ -0,0 +1,5 @@
+numbers = [1,2,3,4,5,6,7]
+
+lst = [num*2 for num in numbers if num % 2 == 0 ]
+
+print(lst)
