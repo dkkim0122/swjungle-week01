5003bd9a30d8bc481ece7936c39aee057072e5ef
dkkim0122 <dkkim0122@gmail.com>
2021-11-10 15:47:49 +0900

    2309 문제 해결. 조합을 이용한 풀이

--- baekjoon/2309/dokyung.py
+++ baekjoon/2309/dokyung.py
@@ -1,26 +1,22 @@
 import sys
+from itertools import combinations
 
 dwarfs = [None]*9
 
 for i in range(9):
-    dwarfs[i] = int(input())
-
-every_sum = sum(dwarfs)
-
-flag = False
-
-for i in range(len(dwarfs)):
-    dwarf1 = dwarfs[i]
-    for j in range(i+1, len(dwarfs)):
-        dwarf2 = dwarfs[j]
-        if every_sum - (dwarf1+dwarf2) == 100:
-            dwarfs.remove(dwarf1)
-            dwarfs.remove(dwarf2)
-            flag = True
-            break
-    if flag == True:
-        break
+    dwarfs[i] = int(sys.stdin.readline())
 
 dwarfs.sort()
+
+total_height = sum(dwarfs)
+
+comb = list(combinations(dwarfs, 2))
+
+for dwarf1, dwarf2 in comb:
+    if total_height - (dwarf1+dwarf2) == 100:
+        dwarfs.remove(dwarf1)
+        dwarfs.remove(dwarf2)
+        break  # 얘를 안 해줘서 틀렸다라....
+
 for i in dwarfs:
     print(i)
