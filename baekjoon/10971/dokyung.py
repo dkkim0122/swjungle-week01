fa888d99e41015883464abaea904bdbb788c3eac
dkkim0122 <dkkim0122@gmail.com>
2021-11-10 21:57:55 +0900

    10971 문제 해결. 최대한 불필요한 반복문과 메모리를 줄이기

--- baekjoon/10971/dokyung.py
+++ baekjoon/10971/dokyung.py
@@ -3,28 +3,43 @@ import sys
 input = sys.stdin.readline
 
 def get_cost(a: list) -> list:
-    cost_list = []
     ways = list(permutations(a))
+    min = sys.maxsize  #  그냥 정수로서 가장 큰 값 넣어주었다.
 
     for way in ways:
-        cost = 0
+        if costs[way[-1]][way[0]] == 0:
+            continue
         n = len(way)
-        for i in range(n-1):
-            cost += w[way[i]][way[i+1]]
-        cost += w[way[n-1]][way[0]]
-        cost_list.append(cost) 
+        cost = 0
+        flag = True
 
-    return cost_list
+        for i in range(n-1):
+            camefrom = way[i]
+            goto = way[i+1]
+            if costs[camefrom][goto] == 0:
+                flag = False
+                break
+            cost += costs[camefrom][goto]
+            if cost >= min:
+                flag = False
+                break
+        if flag == False:
+            continue 
+        cost += costs[way[-1]][way[0]]
+        if cost < min:
+            min = cost
+
+    return min
 
 N = int(input())
 
-w = [None] * N
+costs = [None] * N
 a = [i for i in range(N)]
 
 for i in range(N):
-    w[i] = list(map(int, input().split()))
+    costs[i] = list(map(int, input().split()))
 
-cost_list = get_cost(a)
+print(costs)
 
-print(min(cost_list))
+print(get_cost(a))
 
